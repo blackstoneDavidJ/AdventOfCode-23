049a555d5f2e53e812656cd66281768888ab9790
dblac <dblackstone107@gmail.com>
2023-12-04 10:25:49 -0500

    C4 part 1 done

--- src/main/java/advent/C4.java
+++ src/main/java/advent/C4.java
@@ -9,33 +9,25 @@ public class C4
     private static final String PATH = "src/inputfiles/c423.txt";
     public static void main(String[] args) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File(PATH));
+        int sum = 0;
         while(scanner.hasNextLine()) {
             String line = scanner.nextLine();
             String[] cards = line.split("\\|");
-            HashMap<String, Integer> winningCards = buildMap(cards);
+            String[] winningCards2 = Arrays.stream(cards[0].split(":")[1].split(" "))
+                    .filter(s -> !s.isEmpty())
+                    .toArray(String[]::new);
             ArrayList<String> bagCards = new ArrayList<>(List.of(cards[1].split(" ")));
-            for(String card : winningCards.keySet()) {
-                System.out.println(card);
+            int total = 0;
+            for(String card : winningCards2) {
                 if (bagCards.contains(card)) {
-                    if (winningCards.get(card) == 0)
-                        winningCards.put(card, winningCards.get(card) + 1);
-                    else
-                        winningCards.put(card, winningCards.get(card) * 2);
+                    if (total == 0) total = 1;
+                    else total *= 2;
                 }
             }
-            int total = 0;
-            for(String card : winningCards.keySet())
-            {
-                total += winningCards.get(card);
-            }
-            System.out.println(total);
+            sum += total;
         }
-        scanner.close();
-    }
+        System.out.println(sum);
 
-    private static HashMap<String, Integer> buildMap(String[] cards) {
-        HashMap<String, Integer> map = new HashMap<>();
-        for (String card : cards) map.put(card, 0);
-        return map;
+        scanner.close();
     }
 }
