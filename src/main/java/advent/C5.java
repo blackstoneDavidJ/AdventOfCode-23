ac3b8935a20ff605ad2c3b7109836e646f6a3dd4
David <dblackstone107@gmail.com>
2023-12-19 21:42:28 -0500

    c5 p1 works better

--- src/main/java/advent/C5.java
+++ src/main/java/advent/C5.java
@@ -1,6 +1,5 @@
 package advent;
 
-import javax.print.attribute.standard.PresentationDirection;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
@@ -15,33 +14,26 @@ public class C5 {
         HashMap<String, List<String>> map = makeMapHelper(labels, scanner);
         long[] rts = map.get("seeds").stream().mapToLong(Long::parseLong).toArray();
         Pair[] results = Arrays.stream(rts).mapToObj(Pair::new).toArray(Pair[]::new);
-        for(String label : labels) {
-            Pair[] newResults = getNextMapResults(results, map, label);
-            for(Pair p : newResults) {
-                System.out.println(p.n);
-                p.solved = false;
-            }
-            System.out.println("--");
-        }
-
-        System.out.println(Arrays.stream(results));
+        Arrays.stream(labels).forEach(l -> {
+            getNextMapResults(results, map, l);
+            Arrays.stream(results).forEach(p -> p.solved = false);
+        });
+        Arrays.stream(results).min(Comparator.comparingLong(p -> p.n)).ifPresent(System.out::println);
     }
-    private static Pair[] getNextMapResults(Pair[] input, HashMap<String, List<String>> map, String label) {
+    private static void getNextMapResults(Pair[] input, HashMap<String, List<String>> map, String label) {
         List<String> list = map.get(label);
         for(String param : list) {
             long[] p = Arrays.stream(param.split(" ")).mapToLong(Long::parseLong).toArray();
-            for(int i = 0; i < input.length; i++) {
-                long seed = input[i].n;
+            for (Pair pair : input) {
+                long seed = pair.n;
                 long offset = seed - p[1];
-                if (seed >= p[1] && offset <= p[2] && !input[i].solved) {
-                    input[i].n = p[0] + offset;
-                    input[i].solved = true;
+                if (seed >= p[1] && offset <= p[2] && !pair.solved) {
+                    pair.n = p[0] + offset;
+                    pair.solved = true;
                 }
             }
         }
-        return input;
     }
-
     private static HashMap<String, List<String>> makeMapHelper(String[] labels, Scanner scanner) {
         HashMap<String, List<String>> maps = new HashMap<>();
         String line = scanner.nextLine();
@@ -69,5 +61,7 @@ public class C5 {
         return list;
     }
     private static class Pair { boolean solved; long n; Pair(long n) { this.n = n; }
+        @Override
+        public String toString() { return "value: " +n; }
     }
 }
